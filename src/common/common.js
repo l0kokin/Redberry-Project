604c8f19b43a5d786d87f398c336b7270cefaec3
l0kokin <sali.gogishvili1@gmail.com>
2024-09-11 22:31:08 +0400

    hide api key in env

--- src/common/common.js
+++ src/common/common.js
@@ -1,10 +1,10 @@
-const API_TOKEN = "";
 const API_URL = "https://api.real-estate-manager.redberryinternship.ge/api";
 
 const fetchData = async (path) => {
   try {
+    console.log(process.env);
     const response = await fetch(`${API_URL}/${path}`, {
-      headers: { Authorization: `Bearer ${API_TOKEN}` },
+      headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` },
     });
     const data = await response.json();
     return data;
