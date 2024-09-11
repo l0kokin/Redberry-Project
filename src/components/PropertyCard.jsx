3a6e767d91433b4eb9cbb638aff182dafc990ce2
l0kokin <sali.gogishvili1@gmail.com>
2024-09-11 21:26:28 +0400

    add property fetching functionality

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,12 +1,32 @@
 import { Card } from "./PropertyCardStyles";
-import Img from "../images/randomImg.png";
+import { ReactComponent as LocationIcon } from "../icons/location.svg";
+import { ReactComponent as BedIcon } from "../icons/bed.svg";
+import { ReactComponent as AreaIcon } from "../icons/area.svg";
+import { ReactComponent as ZipCodeIcon } from "../icons/zip.svg";
 
-function PropertyCard() {
+function PropertyCard({ property }) {
   return (
     <Card>
-      <img src={Img} alt="dummy img" />
-      <p className="price">80, 458 ₾</p>
-      <p className="info">თბილისი, ი. ჭავჭავაძის 53</p>
+      <img src={property.image} alt="dummy img" />
+      <p className="price">{property.price}</p>
+      <p className="info">
+        <LocationIcon />
+        {property.address}
+      </p>
+      <div>
+        <div>
+          <BedIcon />
+          {property.bedrooms}
+        </div>
+        <div>
+          <AreaIcon />
+          {property.area}
+        </div>
+        <div>
+          <ZipCodeIcon />
+          {property.zip_code}
+        </div>
+      </div>
     </Card>
   );
 }
--- src/pages/PropertyList.jsx
+++ src/pages/PropertyList.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import PropertyCard from "../components/PropertyCard";
 
+const API_TOKEN = "9cfc13f4-6347-4be5-9ee8-0c174ea87e63";
+const API_URL = "https://api.real-estate-manager.redberryinternship.ge/api";
+
 function PropertyList() {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const response = await fetch(`${API_URL}/real-estates`, {
+          headers: { Authorization: `Bearer ${API_TOKEN}` },
+        });
+        const data = await response.json();
+        setProperties(data);
+      } catch (error) {
+        console.error("Error fetching properties:", error);
+      }
+    };
+
+    fetchProperties();
+  }, []);
+
   return (
     <div>
-      <PropertyCard />
+      {properties.map((property) => (
+        <PropertyCard key={property.id} property={property} />
+      ))}
     </div>
   );
 }
