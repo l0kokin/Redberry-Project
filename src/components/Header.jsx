151a80cbbfae5b7d2a4a1f41ca8744a2bc416dee
l0kokin <sali.gogishvili1@gmail.com>
2024-09-12 14:06:55 +0400

    finish styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import Logo from "../images/logo-redberry.png";
 import styled from "styled-components";
+import { colors } from "../colors";
 
 function Header() {
   return (
@@ -12,7 +13,13 @@ function Header() {
 export default Header;
 
 const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
   width: 100vw;
   height: 2.4rem;
-  margin: 3.8rem 0rem 11rem;
+  margin-left: 0rem !important;
+  padding: 4.8rem 0rem 4.8rem 16.2rem;
+  border-bottom: 1px solid ${colors.light_grey};
+  margin-bottom: 7.7rem;
+  margin-left: -16.2rem !important;
 `;
